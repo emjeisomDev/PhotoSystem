8238b167e08c3f6e96f9ab25ff6bc92fba1dc0d8
emjeisomDev <emjeisom.dev@gmail.com>
2023-07-02 13:09:15 -0300

    sidenav refactoring Part I

--- PhotoSystem-Angular/src/app/navigation/sidenav-comp/sidenav/sidenav.component.ts
+++ PhotoSystem-Angular/src/app/navigation/sidenav-comp/sidenav/sidenav.component.ts
@@ -1,30 +1,31 @@
 import { Component } from '@angular/core';
-import { fadeInOut , rotate} from '../../animations';
+import { fadeInOut, rotate } from '../../animations';
 
 @Component({
-  selector: 'app-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss'],
-  animations: [fadeInOut , rotate]
+   selector: 'app-sidenav',
+   templateUrl: './sidenav.component.html',
+   styleUrls: ['./sidenav.component.scss'],
+   animations: [fadeInOut, rotate]
 })
 export class SidenavComponent {
-  public collapsed: boolean = true;
-  public sidenavClosed: boolean = false;
-  public sidenavResizeTo!: number;
+   public collapsed: boolean = true;
+   public sidenavClosed: boolean = false;
+   public sidenavResizeTo!: number;
 
-  closeMenu():void{
-    this.sidenavClosed = true;
-    this.verifyAppLogoWidth();
-    console.log(`Is sidenav close? ${this.sidenavClosed}. What's sidenav size? ${this.sidenavResizeTo}`);
-  }
+   closeMenu(): void {
+      this.sidenavClosed = true;
+      this.verifyAppLogoWidth();
+      console.log(`Is sidenav close? ${this.sidenavClosed}. What's sidenav size? ${this.sidenavResizeTo}`);
+   }
 
-  public toggleMenu(): void{
-    this.sidenavClosed = !this.sidenavClosed;
-    this.verifyAppLogoWidth();
-  }
+   public toggleMenu(): void {
+      this.sidenavClosed = !this.sidenavClosed;
+      this.verifyAppLogoWidth();
+      console.log(`Is sidenav close? ${this.sidenavClosed}. What's sidenav size? ${this.sidenavResizeTo}`);
+   }
 
-  public verifyAppLogoWidth(): void{
-    this.sidenavResizeTo = this.sidenavClosed ? 48 : 240;
-  }
+   public verifyAppLogoWidth(): void {
+      this.sidenavResizeTo = this.sidenavClosed ? 48 : 240;
+   }
 
 }
