55c28509c75a665ad17c43f8b57f0f4eafe08190
emjeisomDev <emjeisom.dev@gmail.com>
2023-07-13 14:11:48 -0300

    Configured power-of-attorney form

--- PhotoSystem-Angular/src/app/shared/app-material/app-material.module.ts
+++ PhotoSystem-Angular/src/app/shared/app-material/app-material.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
+
+
 
 
 
@@ -26,7 +30,9 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
     MatFormFieldModule,
     MatRadioModule,
     MatInputModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatSelectModule,
+    MatCheckboxModule
   ]
 })
 export class AppMaterialModule { }
